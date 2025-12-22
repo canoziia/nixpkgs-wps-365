55b3e7eee235c7b37864a4055aef9961735017ad
canoziia <canoziia@kawaii.net>
2025-12-22 22:16:50 +0800

    fix wps login

--- default.nix
+++ default.nix
@@ -4,35 +4,28 @@
   fetchurl,
   dpkg,
   autoPatchelfHook,
-  makeWrapper,
   alsa-lib,
-  at-spi2-core,
-  libbsd,
-  cairo,
+  libjpeg,
+  libtool,
+  libxkbcommon,
+  nss,
+  nspr,
+  udev,
+  gtk3,
+  libgbm,
+  libusb1,
+  unixODBC,
+  libmysqlclient,
+  qt5,
+  xorg,
   cups,
   dbus,
-  expat,
-  fontconfig,
-  freetype,
-  gdk-pixbuf,
-  glib,
-  gtk3,
-  libdrm,
-  libglvnd,
-  libtool,
-  libuuid,
-  libxcb,
-  libxkbcommon,
-  mesa,
-  nspr,
-  nss,
   pango,
-  xorg,
-  zlib,
-  qt5,
-  libusb1,
+  openssl,
+  glib-networking,
   libpulseaudio,
-  bzip2,
+  libbsd,
+  coreutils,
 }:
 
 let
@@ -62,106 +55,102 @@ stdenv.mkDerivation {
   nativeBuildInputs = [
     dpkg
     autoPatchelfHook
-    makeWrapper
-    qt5.wrapQtAppsHook
   ];
 
   buildInputs = [
     alsa-lib
-    at-spi2-core
-    libbsd
-    cairo
-    cups
-    dbus
-    expat
-    fontconfig
-    freetype
-    gdk-pixbuf
-    glib
-    gtk3
-    libdrm
-    libglvnd
+    libjpeg
     libtool
-    libuuid
-    libxcb
     libxkbcommon
-    mesa
-    nspr
     nss
-    pango
+    nspr
+    udev
+    gtk3
+    libgbm
+    libusb1
+    unixODBC
+    qt5.qtbase
+    xorg.libXdamage
+    xorg.libXtst
+    xorg.libXv
     xorg.libX11
+    xorg.libXext
+    xorg.libxcb
     xorg.libXcomposite
     xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
     xorg.libXfixes
     xorg.libXi
     xorg.libXrandr
     xorg.libXrender
     xorg.libXScrnSaver
-    xorg.libXtst
-    xorg.libXv
-    zlib
-    libusb1
+    xorg.libXxf86vm
     libpulseaudio
-    bzip2
+    libbsd
+    openssl
+    glib-networking
   ];
+
+  # Official package uses this to prevent Qt conflicts
+  dontWrapQtApps = true;
 
   runtimeDependencies = [
-    libbsd
+    cups
+    dbus
+    pango
+    libmysqlclient
   ];
 
-  autoPatchelfIgnoreMissingDeps = [
-    "libmysqlclient.so.18"
-    "libpeony.so.3"
-  ];
-
+  # Remove unneeded files and broken libs
+  # Aligning with official package cleanup
   unpackPhase = ''
     dpkg-deb -x $src .
+
+    # Remove standard unneeded directories
+    rm -rf usr/share/{fonts,locale}
+    rm -f usr/bin/misc
+    rm -rf opt/kingsoft/wps-office/{desktops,INSTALL}
+
+    # Remove problematic bundled libraries
+    # Removing libstd++, libgcc_s, libnss, libdbus, libbz2, libjpeg to force system usage
+    # Also removing broken plugins
+    rm -f opt/kingsoft/wps-office/office6/lib{peony-wpsprint-menu-plugin,bz2,jpeg,stdc++,gcc_s,odbc*,nss*,dbus-1,ssl,crypto}.so*
   '';
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out
-    cp -r opt/kingsoft/wps-office/* $out/
 
-    # Fixup path in wps executable scripts
-    # The deb often puts binaries in /opt/kingsoft/wps-office/office6
-    # and scripts in /usr/bin. We need to handle this.
+    cp -r opt $out
+    cp -r usr/{bin,share} $out
 
-    # Check structure
-    ls -R $out
-
-    # Remove broken bundled libraries that we provide
-    rm -f $out/office6/libbz2.so*
-
-    # Usually the deb installs specific launch scripts.
-    # Wps 365 structure might be slighty different from standard community version.
-
-    mkdir -p $out/bin
-
-    # Link main executables
-    # Adjust valid executables based on extracted content
-    for app in wps wpp et pdf; do
-      if [ -f "$out/office6/$app" ]; then
-        ln -s $out/office6/$app $out/bin/$app
-      fi
+    # Fixup paths in launchers
+    for i in $out/bin/*; do
+      substituteInPlace $i \
+        --replace "/opt/kingsoft/wps-office" "$out/opt/kingsoft/wps-office"
     done
 
-    # Copy icons and desktop files if they exist in usr/share
-    if [ -d usr/share ]; then
-      cp -r usr/share $out/
-    fi
-
-    # Fix desktop files
-    if [ -d $out/share/applications ]; then
-      substituteInPlace $out/share/applications/*.desktop \
-        --replace "/opt/kingsoft/wps-office" "$out" \
-        --replace "/usr/bin" "$out/bin"
-    fi
+    # Fixup desktop files
+    for i in $out/share/applications/*; do
+      substituteInPlace $i \
+        --replace "/usr/bin" "$out/bin" \
+        --replace "/opt/kingsoft/wps-office" "$out/opt/kingsoft/wps-office"
+    done
 
     runHook postInstall
+  '';
+
+  preFixup = ''
+    # Official fixes
+    if [ -f $out/opt/kingsoft/wps-office/office6/addons/cef/libcef.so ]; then
+      patchelf --add-needed libudev.so.1 $out/opt/kingsoft/wps-office/office6/addons/cef/libcef.so
+    fi
+
+    # Fix mysql linkage if exists
+    if [ -f $out/opt/kingsoft/wps-office/office6/libFontWatermark.so ]; then
+      patchelf --replace-needed libmysqlclient.so.18 libmysqlclient.so $out/opt/kingsoft/wps-office/office6/libFontWatermark.so || true
+      patchelf --add-rpath ${libmysqlclient}/lib/mariadb $out/opt/kingsoft/wps-office/office6/libFontWatermark.so || true
+    fi
   '';
 
   meta = with lib; {
